d44687a603b03e33894da227ead8bd74e6323368
JoyaAlbert <91436479+JoyaAlbert@users.noreply.github.com>
2023-12-10 22:33:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	int fd = open("testTexts/dracula.txt", O_RDONLY);
+	int fd = open("test.txt", O_RDONLY);
 	for(int i = 0; i < 20; i++)
 		printf("%s", get_next_line(fd));
 	return 0;
