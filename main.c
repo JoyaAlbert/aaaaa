cf3f44e9229eea6500be447b221dbb5f10ba7ef6
Alberto Joya Pincu <ajoya-pi@c3r14s3.42madrid.com>
2023-12-27 15:33:35 +0100

    ya

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-	int fd = open("testTexts/big.txt", O_RDONLY);
-	for(int i = 0; i < 3; i++)
+	int fd = open("testTexts/test.txt", O_RDONLY);
+	for(int i = 0; i < 15; i++)
 		printf("%s", get_next_line(fd));
+	system("leaks -q a.out");
 	return 0;
 }
