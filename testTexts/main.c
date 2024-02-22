abb536e427a4985069b1090c7dbea48f15b7d3aa
JoyaAlbert <albertojoyapincu2@gmail.com>
2024-02-22 13:43:15 +0100

    a

--- testTexts/main.c
+++ testTexts/main.c
@@ -4,9 +4,8 @@ int	main(void)
 {
 	int		fd_1;
 	char	*line;
-
-
-  fd_1 = open("testTexts/dracula.txt", O_RDONLY);
+	
+	fd_1 = open("testTexts/dracula.txt", O_RDONLY);
 	while (1)
 	{
 		line = get_next_line(fd_1);
